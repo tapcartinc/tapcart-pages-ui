49e4831169b06634943a3059731abb1c514125dc
Justin McLaren <justinmclaren63@gmail.com>
2023-05-25 17:15:52 -0700

    fix: remove 'button' styles

--- src/lib/components/atoms/at-button/index.tsx
+++ src/lib/components/atoms/at-button/index.tsx
@@ -22,7 +22,7 @@ export interface AtButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export const AtButton = ({ label, variant = 'PRIMARY', isDisabled = false, onClick }: AtButtonProps) => {
   return (
     <button
-      className={`transition-colors px-6 py-2 rounded-md ${variantClasses[variant]} ${
+      className={`transition-colors px-6 py-2 ${variantClasses[variant]} ${
         isDisabled ? 'bg-gray-300 text-slate-600 cursor-not-allowed pointer-events-none' : ''
       }`}
       onClick={isDisabled ? onClick : undefined}
